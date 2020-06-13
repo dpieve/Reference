04593c08ab9b327ba78578ac1598a51c85eeea37
Pieve <pievetp@gmail.com>
2020-06-13 17:02:26 -0300

    Update compile&run.sh

--- tricks/compile&run.sh
+++ tricks/compile&run.sh
@@ -6,7 +6,7 @@ Windows
 g++ -o program program.cpp && program.exe <input.txt
 
 Linux
-g++ program.cpp -o output && ./output.out <input.txt
+g++ -o A A.cpp && ./A <in.txt
 
 With flags
 g++ -std=c++11 -O2 -Wall A.cpp -o a && ./a.out <in.txt
