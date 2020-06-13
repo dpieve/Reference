80425a604139eef95a77c4a78c2ba67b92f44093
Pieve <pievetp@gmail.com>
2020-06-13 15:06:57 -0300

    Create compile&run.cpp

---
+++ tricks/compile&run.cpp
@@ -0,0 +1,11 @@
+Compile and Run C++
+
+Windows 
+g++ -o program program.cpp && program.exe <input.txt
+
+Linux
+g++ program.cpp -o output && ./output.out <input.txt
+
+With flags
+g++ -std=c++11 -O2 -Wall A.cpp -o a && ./a.out <in.txt
+
