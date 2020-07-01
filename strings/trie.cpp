eb55f7c038828ea0e39f1a2a58048e9aabd83177
Pieve <pievetp@gmail.com>
2020-07-01 19:13:33 -0300

    Update trie.cpp

--- strings/trie.cpp
+++ strings/trie.cpp
@@ -23,29 +23,3 @@ int search(const string S) {
 	}
 	return finish[idx];
 }
-
-int main() {
-	
-	ios_base :: sync_with_stdio(0); cin.tie(NULL);
-
-	int N;
-	cin >> N;
-
-	for(int i = 0; i < N; ++i) {
-		string S;
-		cin >> S;
-		insert(S);
-	}
-
-	int M;
-	cin >> M;
-
-	for(int i = 0; i < M; ++i) {
-		string T;
-		cin >> T;
-
-		cout << (search(T) ? "Y" : "N") << '\n';
-	}
-
-	return 0;	
-}
