0e83caa521ec73c77f6c196a8980b4420c7905f7
Pieve <pievetp@gmail.com>
2020-07-23 15:31:54 -0300

    Update seg_tree_point_upd.cpp

--- data_structures/seg_tree_point_upd.cpp
+++ data_structures/seg_tree_point_upd.cpp
@@ -1,8 +1,10 @@
 const int N = 200000 + 5;
 
 int t[4*N], a[N];
+ 
+// build (1, 0, n); 
+// index of the array starts with zero
 
-// build (1, 1, n);
 void build(int i, int l, int r) {							
 	if (l == r) {
 		t[i] = a[l];
@@ -15,7 +17,7 @@ void build(int i, int l, int r) {
 	}
 }
 
-// query (1, 1, n, x, y);
+// query (1, 0, n, x, y);
 int query(int i, int l, int r, int ql, int qr) {
 	if (ql <= l && r <= qr) return t[i];
 	if (qr < l || r < ql) return 0;
@@ -25,7 +27,7 @@ int query(int i, int l, int r, int ql, int qr) {
 		 		+ query(2*i+1, m+1, r, ql, qr);
 }
 
-//a[p] = x // update (1, 1, n, p, x)
+//a[p] = x // update (1, 0, n, p, x)
 void update(int i, int l, int r, int p, int x) {
 	if (l == r) {
 		t[i] = x;
