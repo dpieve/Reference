acec578ada813f2a275b41e95bb7bc08e501c4a3
Pieve <pievetp@gmail.com>
2020-08-06 16:15:00 -0300

    Update c_plus_plus.cpp

--- tricks/c_plus_plus.cpp
+++ tricks/c_plus_plus.cpp
@@ -33,5 +33,10 @@ int main () {
 	string str = "................";
 	std::string str2 = str.substr (3,5); // 3 = start, 5 = len
 	
+	//binary search pair
+	x.lower_bound({first, -inf}); // iterator to the first pair with value >= first. -INF get the first
+	x.upper_bound({first,  inf}); // iterator to the first pair with value  > first  +INF get the next when tie happen
+	
+	
 	return 0;
 }
